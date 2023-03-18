bc8b591f64bbf1bd1662b22723ef7a9869cb98d1
stracciatella27 <schoko.bombe44@gmail.com>
2023-03-18 15:00:50 +0100

    set roboto font as global

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,5 +1,10 @@
+import { Center, Flex } from "@chakra-ui/react";
+import { faSpider } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Logo() {
-    return <div>
-        logo
-    </div>
+    return <Flex align={"center"} justify={"center"} gap={2} padding={5}>
+        <FontAwesomeIcon icon={faSpider} size={"2x"} color="#E63946" />
+        <h2 className="text-3xl">Crawlr<span className="text-red">.</span></h2>
+    </Flex>
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,28 @@
 import "@/styles/globals.css";
-import "@fortawesome/fontawesome-svg-core/styles.css"; 
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import { Roboto } from "next/font/google";
 import AppBar from "@/components/AppBar";
 import theme from "@/theme";
+import { Roboto } from 'next/font/google'
 
-const roboto = Roboto({
-  weight: "400",
-  subsets: ["latin"],
-});
+const roboto = Roboto({ weight: ['400', '700'],subsets: ['latin'] })
 
 // 3. Pass the `theme` prop to the `ChakraProvider`
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <>
+    <style jsx global>{`
+        html, body {
+          font-family: ${roboto.style.fontFamily} !important;
+        }
+      `}</style>
     <ChakraProvider theme={theme}>
         <AppBar />
-        <main className={roboto.className}>
+        <main>
           <Component {...pageProps} />
         </main>
     </ChakraProvider>
+    </>
   );
 }
