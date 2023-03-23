c60666567bd6c658d5229557a18b1b72b1997034
stracciatella27 <schoko.bombe44@gmail.com>
2023-03-23 20:05:46 +0100

    add metaScraper endpoint

--- pages/api/getMetaInformation/[url].ts
+++ pages/api/getMetaInformation/[url].ts
@@ -1,22 +1,43 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
-import url from "url";
+import validUrl from "valid-url";
 
-type Data = {
-  url: any;
-};
+const metascraper = require("metascraper")([
+  require("metascraper-author")(),
+  require("metascraper-description")(),
+  require("metascraper-image")(),
+  require("metascraper-title")(),
+  require("metascraper-logo-favicon")()
+]);
+
+type metaData = {
+  author: string | null;
+  description: string | null;
+  image: string | null;
+  title: string | null;
+  logo: string | null;
+} | "Invalid URL";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<metaData>
 ) {
-   const data = await axios({
+  const query = req.query.url?.toString() || "";
+
+  if (!validUrl.isWebUri(query)) {
+    return res.status(400).json("Invalid URL");
+  }
+
+  const {data} = await axios({
     method: "get",
-    url: "https://www.google.com/",
-   });
+    url: query,
+  });
 
-   const newUrl = new url.URL('www.google.at')
+  const metadata: metaData = await metascraper({
+    html: data,
+    url: query.toString(),
+  });
 
-  res.status(200).json({ url: newUrl });
+  res.status(200).json(metadata);
 }
